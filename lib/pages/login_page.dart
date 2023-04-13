21eb893091ae1df79c447445fcce0839c6c707d5
FernandoAguero <fer@Fernandos-MacBook-Air.local>
2023-04-12 20:24:57 -0600

    fixed stateful bug

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-class LoginPage {
+class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginPageState extends LoginPage {
+class _LoginPageState extends State<LoginPage> {
   final formkey = GlobalKey<FormState>();
   final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
@@ -17,24 +17,34 @@ class _LoginPageState extends LoginPage {
       body: SingleChildScrollView(
           child: Form(
         key: formkey,
-        child: Column(
-          children: <Widget>[
-            TextFormField(
-              controller: _usernameController,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                  hintText: "Enter email", labelText: "Usuario"),
-            ),
-            TextFormField(
-              controller: _passwordController,
-              keyboardType: TextInputType.text,
-              obscureText: true,
-              decoration: InputDecoration(
-                hintText: "Enter password",
-                labelText: "Password",
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                controller: _usernameController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                    hintText: "Enter email", labelText: "Usuario"),
               ),
-            )
-          ],
+              TextFormField(
+                controller: _passwordController,
+                keyboardType: TextInputType.text,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "Enter password",
+                  labelText: "Password",
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text("Iniciar"),
+              )
+            ],
+          ),
         ),
       )),
     );
