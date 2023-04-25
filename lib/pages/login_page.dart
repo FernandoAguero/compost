f61443c70ed84076371f3c2788c4d3f9ed22f57d
FernandoAguero <fer@Fernandos-MacBook-Air.local>
2023-04-25 17:40:35 -0600

    image alignment fix

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -15,43 +15,51 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
         appBar: AppBar(title: Text("Login Page")),
         body: Stack(
+          fit: StackFit.expand,
           children: <Widget>[
-            Image.asset("assets/images/logo.png", fit: BoxFit.cover),
-            const SizedBox(height: 600),
-            SingleChildScrollView(
-                child: Form(
-              key: formkey,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    TextFormField(
-                      controller: _usernameController,
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(
-                          hintText: "Enter email", labelText: "Usuario"),
-                    ),
-                    TextFormField(
-                      controller: _passwordController,
-                      keyboardType: TextInputType.text,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        hintText: "Enter password",
-                        labelText: "Password",
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 60,
-                    ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: const Text("Iniciar"),
-                    )
-                  ],
-                ),
+            Align(
+              alignment: AlignmentDirectional.topStart,
+              child: Image.asset(
+                "assets/images/logo.png",
+                //fit: BoxFit.cover,
               ),
-            ))
+            ),
+            Align(
+              child: SingleChildScrollView(
+                  child: Form(
+                key: formkey,
+                child: Padding(
+                  padding: const EdgeInsets.all(26.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      TextFormField(
+                        controller: _usernameController,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                            hintText: "Enter email", labelText: "Usuario"),
+                      ),
+                      TextFormField(
+                        controller: _passwordController,
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: "Enter password",
+                          labelText: "Password",
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 60,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: const Text("Iniciar"),
+                      )
+                    ],
+                  ),
+                ),
+              )),
+            )
           ],
         ));
   }
